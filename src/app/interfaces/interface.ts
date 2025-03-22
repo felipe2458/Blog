443ea4c1676c125456e16b8edddd262f8ac61769
felipe2458 <luizfelipealvesdematos10@gmail.com>
2025-03-22 19:32:40 -0300

    finalizando o projeto (Leia o README)

---
+++ src/app/interfaces/interface.ts
@@ -0,0 +1,4 @@
+export interface likesBlogs {
+  id: number;
+  like: boolean;
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { likesBlogs } from '../../interfaces/interface';
 
 @Component({
   selector: 'app-home',
@@ -9,11 +10,33 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor(){
+    const likesBlogs = [
+      { like: localStorage.getItem('likesBlogs0') },
+      { like: localStorage.getItem('likesBlogs1') },
+      { like: localStorage.getItem('likesBlogs2') },
+    ]
+
+    this.likesBlogs[0].like = likesBlogs[0].like ? JSON.parse(likesBlogs[0].like) : false;
+    this.likesBlogs[1].like = likesBlogs[1].like ? JSON.parse(likesBlogs[1].like) : false;
+    this.likesBlogs[2].like = likesBlogs[2].like ? JSON.parse(likesBlogs[2].like) : false;
+  }
+
   search: string = '';
   valueTop: number = 10;
 
   checkSearchEmpty(focus: boolean, blur: boolean): void {
     if(this.search === '' && focus) this.valueTop = -20;
     if(this.search === '' && blur) this.valueTop = 10;
+  }
+
+  likesBlogs: likesBlogs[] = [
+    { id: 0, like: false },
+    { id: 1, like: false },
+    { id: 2, like: false },
+  ];
+
+  saveLikeStorage(id: number): void{
+    localStorage.setItem(`likesBlogs${id}`, JSON.stringify(this.likesBlogs[id].like));
   }
 }
