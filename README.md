b6a06a1e8a8b1a4ffd83e348a25df33e526800bd
felipe2458 <luizfelipealvesdematos10@gmail.com>
2025-03-23 01:39:03 -0300

    finalizando a responsividade

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 Esse projeto foi desenvolvido com o Angular 19.
 
 Creditos do desafio de frontend: https://github.com/iuricode/desafios-frontend
-
-NOTA: O projeto está praticamente finalizado, mas ainda falta a parte de responsividade.
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { likesBlogs } from '../../interfaces/interface';
@@ -20,14 +20,22 @@ export class HomeComponent {
     this.likesBlogs[0].like = likesBlogs[0].like ? JSON.parse(likesBlogs[0].like) : false;
     this.likesBlogs[1].like = likesBlogs[1].like ? JSON.parse(likesBlogs[1].like) : false;
     this.likesBlogs[2].like = likesBlogs[2].like ? JSON.parse(likesBlogs[2].like) : false;
+
+    this.onResize();
   }
 
   search: string = '';
   valueTop: number = 10;
+  screenWidth: number = window.innerWidth;
 
   checkSearchEmpty(focus: boolean, blur: boolean): void {
-    if(this.search === '' && focus) this.valueTop = -20;
-    if(this.search === '' && blur) this.valueTop = 10;
+    if(this.screenWidth >= 800){
+      if(this.search === '' && focus) this.valueTop = -20;
+      if(this.search === '' && blur) this.valueTop = 10;
+    }else{
+      if(this.search === '' && focus) this.valueTop = -20;
+      if(this.search === '' && blur) this.valueTop = 13;
+    }
   }
 
   likesBlogs: likesBlogs[] = [
@@ -38,5 +46,11 @@ export class HomeComponent {
 
   saveLikeStorage(id: number): void{
     localStorage.setItem(`likesBlogs${id}`, JSON.stringify(this.likesBlogs[id].like));
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(): void{
+    this.screenWidth = window.innerWidth;
+    this.valueTop = 13;
   }
 }
